5d02a2944357444d91dabf66048c7386f72cf2b7
CPX <chinnathorn.p@gmail.com>
2022-02-11 11:39:49 +0700

    Fix bug

--- src/App.js
+++ src/App.js
@@ -156,148 +156,149 @@ function App() {
     window.open('//bnk48fan.cpxdev.tk/ge3', '_blank').focus();
   }
 
-  return (
+  if (uri != '') {
+    return (
  
 
-<div> 
-    {uri != '' && (
-      <BrowserRouter>
-      <AppBar position="sticky" className='bnktheme'>
-          <Toolbar>
-            {open == false && (
-            <IconButton onClick={() => setOpen(true)} edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            )}
-            <div onClick={()=> window.location.href = "/"}>
-              <Typography variant='h5' className='title'>
-               CGM48 Fans Space
-              </Typography>
-            </div>
-              {window.innerWidth > 800 && (
-                <div className={cls.search + ' mt-2'}>
-                <FormControlLabel
-                control={
-                  <Switch
-                    checked={Reduce}
-                    name="reduce"
-                    onChange={()=> ReduceAction()}
-                    color="secondary"
-                  />
-                }
-                label="Reduce Graphic"
-              />
-                </div>
-              )}
-          </Toolbar>
-        </AppBar>
-        <Drawer
-                  className={cls.drawer}
-                  variant="temporary"
-                  color="primary"
-                  anchor="right"
-                  open={open}
-                  classes={{
-                    paper: cls.drawerPaper,
-                  }}
-                >
-                <div className={cls.drawerHeader} position="fixed">
-                  <IconButton onClick={() => setOpen(false)} size="large">
-                    <CloseIcon />
+      <div> 
+          {uri != '' && (
+            <BrowserRouter>
+            <AppBar position="sticky" className='bnktheme'>
+                <Toolbar>
+                  {open == false && (
+                  <IconButton onClick={() => setOpen(true)} edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
                   </IconButton>
-                </div>
-                <Divider />
-                <d onClick={() => setOpen(false)}>
-                <ListItem component={Link} to='/' button>
-                  <ListItemIcon>
-                    <HomeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Home" />
-                </ListItem>
-                <ListItem component={Link} to='/memberlist' button>
-                  <ListItemIcon>
-                    <PeopleIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Members" />
-                </ListItem>
-                <ListItem onClick={() => GoElec()} button>
-                  <ListItemIcon>
-                    <HowToVoteIcon />
-                  </ListItemIcon>
-                  <ListItemText primary='BNK48 12th Single General Election' />
-                </ListItem>
-                <ListItem component={Link} to='/api' button>
-                  <ListItemIcon>
-                    <CodeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary='API' />
-                </ListItem>
-                </d>
-                <Divider />
-                <ListItem onClick={() => {
-                  alert('Region mode will enhance system performance. Current region connection has been referenced by IP address')
-                  window.open('https://status.cpxdev.tk', '_blank').focus()
-                }} button>
-                  <ListItemIcon>
-                    <DnsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={'Region: ' + Fet().nme} />
-                </ListItem>
-                
-                
-                </Drawer>
-                      <BasicSwitch>
-                      <Route exact path="/" render={() => <Home fet={Fet().ul} />} />
-                      <Route exact path="/memberlist" render={() => <MemberList fet={Fet().ul} />} />
-                      <Route exact path="/member" render={() => <MamSam fet={Fet().ul} kamio={kamin} />} />
-                      <Route exact path="/api" render={() => <Api fet={Fet().ul} />} />
-                    </BasicSwitch>
-                
-               
-        <footer className="bg-white text-center pt-2 pb-2 bnktheme">
-          Copyright {new Date().getFullYear()}, CPXDevStudio Allright Reserved
-          <br /> All CGM48 contents are licensed by Independent Artist Management (iAM). We don't affiliated with them. Please don't be to copy and modified contents for any commercial use.
-        </footer>
-        </BrowserRouter>
-    )}
-    {
-      uri == '' && (
-        <div className='text-center mt-5 pt-5'>
-           <img src="https://cdn.jsdelivr.net/gh/cpx2017/cpxcdnbucket@main/main/cpx-circular.svg" width="50px" className='text-center' />
-        Welcome to CGM48 Fan Space, please wait
-      </div>
-      )
-    }
-    <Dialog
-      open={MemberDl}
-      onClose={() => setMemDl(false)}
-      fullWidth={true}
-      maxWidth='sm'
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-  >
-      <DialogTitle id="alert-dialog-title">Are you sure to sign-out</DialogTitle>
-      <DialogContent>
-        Some feature cannot be used when you don't login.
-      </DialogContent>
-      <DialogActions>
-      <GoogleLogout
-      clientId={Client}
-      render={renderProps => (
-        <Button onClick={renderProps.onClick} className="text-danger">
-        Sign out
-    </Button>
-      )}
-      onLogoutSuccess={(e) => Signout(e)}
-      isSignedIn={login}
-    />
-      <Button onClick={() => setMemDl(false)} className="text-dark">
-          Close
-      </Button>
-      </DialogActions>
-  </Dialog>
-    
-  </div> 
+                  )}
+                  <div onClick={()=> window.location.href = "/"}>
+                    <Typography variant='h5' className='title'>
+                     CGM48 Fans Space
+                    </Typography>
+                  </div>
+                    {window.innerWidth > 800 && (
+                      <div className={cls.search + ' mt-2'}>
+                      <FormControlLabel
+                      control={
+                        <Switch
+                          checked={Reduce}
+                          name="reduce"
+                          onChange={()=> ReduceAction()}
+                          color="secondary"
+                        />
+                      }
+                      label="Reduce Graphic"
+                    />
+                      </div>
+                    )}
+                </Toolbar>
+              </AppBar>
+              <Drawer
+                        className={cls.drawer}
+                        variant="temporary"
+                        color="primary"
+                        anchor="right"
+                        open={open}
+                        classes={{
+                          paper: cls.drawerPaper,
+                        }}
+                      >
+                      <div className={cls.drawerHeader} position="fixed">
+                        <IconButton onClick={() => setOpen(false)} size="large">
+                          <CloseIcon />
+                        </IconButton>
+                      </div>
+                      <Divider />
+                      <d onClick={() => setOpen(false)}>
+                      <ListItem component={Link} to='/' button>
+                        <ListItemIcon>
+                          <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                      </ListItem>
+                      <ListItem component={Link} to='/memberlist' button>
+                        <ListItemIcon>
+                          <PeopleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Members" />
+                      </ListItem>
+                      <ListItem onClick={() => GoElec()} button>
+                        <ListItemIcon>
+                          <HowToVoteIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='BNK48 12th Single General Election' />
+                      </ListItem>
+                      <ListItem component={Link} to='/api' button>
+                        <ListItemIcon>
+                          <CodeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='API' />
+                      </ListItem>
+                      </d>
+                      <Divider />
+                      <ListItem onClick={() => {
+                        alert('Region mode will enhance system performance. Current region connection has been referenced by IP address')
+                        window.open('https://status.cpxdev.tk', '_blank').focus()
+                      }} button>
+                        <ListItemIcon>
+                          <DnsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'Region: ' + Fet().nme} />
+                      </ListItem>
+                      
+                      
+                      </Drawer>
+                            <BasicSwitch>
+                            <Route exact path="/" render={() => <Home fet={Fet().ul} />} />
+                            <Route exact path="/memberlist" render={() => <MemberList fet={Fet().ul} />} />
+                            <Route exact path="/member" render={() => <MamSam fet={Fet().ul} kamio={kamin} />} />
+                            <Route exact path="/api" render={() => <Api fet={Fet().ul} />} />
+                          </BasicSwitch>
+                      
+                     
+              <footer className="bg-white text-center pt-2 pb-2 bnktheme">
+                Copyright {new Date().getFullYear()}, CPXDevStudio Allright Reserved
+                <br /> All CGM48 contents are licensed by Independent Artist Management (iAM). We don't affiliated with them. Please don't be to copy and modified contents for any commercial use.
+              </footer>
+              </BrowserRouter>
+          )}
+          <Dialog
+            open={MemberDl}
+            onClose={() => setMemDl(false)}
+            fullWidth={true}
+            maxWidth='sm'
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">Are you sure to sign-out</DialogTitle>
+            <DialogContent>
+              Some feature cannot be used when you don't login.
+            </DialogContent>
+            <DialogActions>
+            <GoogleLogout
+            clientId={Client}
+            render={renderProps => (
+              <Button onClick={renderProps.onClick} className="text-danger">
+              Sign out
+          </Button>
+            )}
+            onLogoutSuccess={(e) => Signout(e)}
+            isSignedIn={login}
+          />
+            <Button onClick={() => setMemDl(false)} className="text-dark">
+                Close
+            </Button>
+            </DialogActions>
+        </Dialog>
+          
+        </div> 
+          )
+  }
+
+    return (
+      <div className='text-center mt-5 pt-5'>
+      <img src="https://cdn.jsdelivr.net/gh/cpx2017/cpxcdnbucket@main/main/cpx-circular.svg" width="50px" className='text-center' />
+   Welcome to CGM48 Fan Space, please wait
+ </div>
     )
 }
 
